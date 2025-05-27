25f7f6b909bc19843e68d3b30c4970a4b2debcaa
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-05-27 22:55:46 +0200

    feat: implement installDependencies function to automate package installation

---
+++ src/steps/installDeps.ts
@@ -0,0 +1,26 @@
+import { detectPackageManager } from '../../utils/packageManager';
+import { runShellCommand } from '../../utils/shell';
+
+export function installDependencies() {
+  const pkgManager = detectPackageManager();
+
+  const deps = ['postgres', 'ley', 'dotenv-safe', 'tsx'];
+
+  console.log(`📦 Installing dependencies using ${pkgManager}...`);
+
+  let installCmd = '';
+  switch (pkgManager) {
+    case 'pnpm':
+      installCmd = `pnpm add ${deps.join(' ')}`;
+      break;
+    case 'yarn':
+      installCmd = `yarn add ${deps.join(' ')}`;
+      break;
+    case 'npm':
+    default:
+      installCmd = `npm install ${deps.join(' ')} --save`;
+      break;
+  }
+
+  runShellCommand(installCmd);
+}
